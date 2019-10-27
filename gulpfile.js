57a5fe7f852c916ca384c1255f4bd650ab633a7e
muhamed91 <rustemi.muhamed@hotmail.com>
2019-10-27 17:53:40 +0100

    Tryin with Minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var uglifycss = require('gulp-uglifycss');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
 
 function StartbrowserSync(){
@@ -20,9 +23,26 @@ function reload(done) {
     done();
 }
 
+
+
+ 
+gulp.task('css', function () {
+  gulp.src('./src/css/**/*.css')
+    .pipe(uglifycss())
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+
 gulp.task('watch', function() {
     StartbrowserSync();
     gulp.watch('./src/*.html', reload);
 });
 
 
+gulp.task('scripts', function() {
+    return gulp.src('./src/css/**/*.css')
+      .pipe(concat('all.css'))
+      .pipe(gulp.dest('./dist/'));
+  });
+  
